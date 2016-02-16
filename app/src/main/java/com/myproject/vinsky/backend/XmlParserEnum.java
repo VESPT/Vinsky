c547a2e866db66a3df8d41d69adbfb82c1476dca
k_ske <keisuke.nakasuga@gmail.com>
2016-02-16 21:06:21 +0900

    create xmlParserClass.

--- app/src/main/java/com/myproject/vinsky/backend/XmlParserEnum.java
+++ app/src/main/java/com/myproject/vinsky/backend/XmlParserEnum.java
@@ -2,19 +2,40 @@ package com.myproject.vinsky.backend;
 
 public enum XmlParserEnum {
 
-    // title
+    /** ?xml */
+    XML("?xml"),
+    /** version */
+    VERSION("version"),
+    /** encoding */
+    ENCODING("encoding"),
+    /** channel */
+    CHANNEL("channel"),
+    /** rdf:about(サイトのRSSのURL or 記事xのURL) */
+    RDF_ABOUT("df:about"),
+    /** rdf:li */
+    RDF_LI("rdf:li"),
+    /** dc:date(作成日時) */
+    DC_DATE("dc:date"),
+    /** dc:language */
+    DC_LANGUAGE("dc:language"),
+    /** item */
+    ITEM("item"),
+    /** title */
     TITLE("title"),
-    // description
+    /** link */
+    LINK("link"),
+    /** description */
     DESCRIPTION("description");
 
-    private String label;
+	/** タグ名 */
+    private String name;
 
-    /** コンストラクタ */
-    XmlParserEnum(String label) {
-        this.label = label;
+    /** Constructor */
+    XmlParserEnum(String name) {
+        this.name = name;
     }
-
-    public String getLabel() {
-        return this.label;
+	
+    public String getName() {
+        return this.name;
     }
 }
